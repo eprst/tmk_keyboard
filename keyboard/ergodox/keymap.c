91b7cf5a26475e242d3b9ea3f479a6e16bed0fbf
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2015-03-12 00:11:29 -0700

    beginnings of my own layout

--- keyboard/ergodox/keymap.c
+++ keyboard/ergodox/keymap.c
@@ -85,6 +85,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "keymap_micro.h"
 #elif defined(KEYMAP_CUB)
 #include "keymap_cub.h"
+#elif defined(KEYMAP_KOS)
+#include "keymap_kos.h"
 #else
 
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -218,7 +220,7 @@ uint8_t keymap_key_to_keycode(uint8_t layer, keypos_t key)
     }
 }
 
-#if defined(KEYMAP_CUB)
+#if defined(KEYMAP_CUB)||defined(KEYMAP_KOS)
 
 // function keymap_fn_to_action will be defined in keymap_cub.h
 
